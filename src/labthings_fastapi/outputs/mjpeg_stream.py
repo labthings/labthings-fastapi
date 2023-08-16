5e47617473a014bb4a726b5043e92806b2619f49
Richard Bowman <richard.bowman@cantab.net>
2023-08-16 14:35:02 +0100

    Move mjpeg stream from examples into module
    This will need testing.

---
+++ src/labthings_fastapi/outputs/mjpeg_stream.py
@@ -0,0 +1,188 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse, HTMLResponse
+from typing import AsyncGenerator, Optional, AsyncContextManager, TYPE_CHECKING
+from contextlib import asynccontextmanager
+from functools import partial
+import threading
+import anyio
+from anyio.from_thread import BlockingPortal
+import logging
+
+if TYPE_CHECKING:
+    from ..thing import Thing
+
+@dataclass
+class RingbufferEntry:
+    """A single entry in a ringbuffer"""
+    frame: bytes
+    timestamp: datetime
+    index: int
+    readers: int = 0
+
+
+class MJPEGStreamResponse(StreamingResponse):
+    media_type = "multipart/x-mixed-replace; boundary=frame"
+    def __init__(self, gen: AsyncGenerator[bytes, None], status_code: int = 200):
+        """A StreamingResponse that streams an MJPEG stream
+        
+        This response is initialised with an async generator that yields `bytes`
+        objects, each of which is a JPEG file. We add the --frame markers and mime
+        types that enable it to work in an `img` tag.
+        
+        NB the `status_code` argument is used by FastAPI to set the status code of 
+        the response in OpenAPI.
+        """
+        self.frame_async_generator = gen
+        StreamingResponse.__init__(
+            self,
+            self.mjpeg_async_generator(),
+            media_type=self.media_type,
+            status_code=status_code,
+        )
+
+    async def mjpeg_async_generator(self) -> AsyncGenerator[bytes, None]:
+        """A generator yielding an MJPEG stream"""
+        async for frame in self.frame_async_generator:
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n"
+            yield frame
+            yield b"\r\n"
+
+
+class MJPEGStream:
+    def __init__(self, ringbuffer_size: int = 10):
+        self._lock = threading.Lock()
+        self.condition = anyio.Condition()
+        self._streaming = False
+        self.reset(ringbuffer_size=ringbuffer_size)
+
+    def reset(self, ringbuffer_size: Optional[int] = None):
+        """Reset the stream and optionally change the ringbuffer size"""
+        with self._lock:
+            self._streaming = True
+            n = ringbuffer_size or len(self._ringbuffer)
+            self._ringbuffer = [
+                RingbufferEntry(
+                    frame=b"",
+                    index=-1,
+                    timestamp=datetime.min,
+                ) 
+                for i in range(n)
+            ]
+            self.last_frame_i = -1
+
+    def stop(self):
+        """Stop the stream"""
+        with self._lock:
+            self._streaming = False
+
+    async def ringbuffer_entry(self, i: int) -> RingbufferEntry:
+        """Return the `i`th frame acquired by the camera"""
+        if i<0:
+            raise ValueError("i must be >= 0")
+        if i < self.last_frame_i - len(self._ringbuffer) + 2:
+            raise ValueError("the ith frame has been overwritten")
+        if i>self.last_frame_i:
+            # TODO: await the ith frame
+            raise ValueError("the ith frame has not yet been acquired")
+        entry = self._ringbuffer[i % len(self._ringbuffer)]
+        if entry.index != i:
+            raise ValueError("the ith frame has been overwritten")
+        return entry
+    
+    @asynccontextmanager
+    async def buffer_for_reading(self, i: int) -> AsyncContextManager[bytes]:
+        """Yields the ith frame as a bytes object"""
+        entry = await self.ringbuffer_entry(i)
+        try:
+            entry.readers += 1
+            yield entry.frame
+        finally:
+            entry.readers -= 1
+
+    async def next_frame(self) -> int:
+        """Wait for the next frame, and return its index"""
+        async with self.condition:
+            await self.condition.wait()
+            return self.last_frame_i
+
+    async def frame_async_generator(self) -> AsyncGenerator[bytes, None]:
+        """A generator that yields frames as bytes"""
+        while self._streaming:
+            try:
+                i = await self.next_frame()
+                async with self.buffer_for_reading(i) as frame:
+                    yield frame
+            except Exception as e:
+                logging.error(f"Error in stream: {e}, stream stopped")
+                return
+            
+    async def mjpeg_stream_response(self) -> MJPEGStreamResponse:
+        """Return a StreamingResponse that streams an MJPEG stream"""
+        return MJPEGStreamResponse(self.frame_async_generator())
+
+    def add_frame(self, frame: bytes, portal: BlockingPortal):
+        """Return the next buffer in the ringbuffer to write to"""
+        with self._lock:
+            entry = self._ringbuffer[(self.last_frame_i + 1) % len(self._ringbuffer)]
+            if entry.readers > 0:
+                raise RuntimeError("Cannot write to ringbuffer while it is being read")
+            entry.timestamp = datetime.now()
+            entry.frame = frame
+            entry.index = self.last_frame_i + 1
+            portal.start_task_soon(self.notify_new_frame, entry.index)
+
+    async def notify_new_frame(self, i):
+        """Notify any waiting tasks that a new frame is available"""
+        async with self.condition:
+            self.last_frame_i = i
+            self.condition.notify_all()
+
+class MJPEGStreamDescriptor:
+    """A descriptor that returns a MJPEGStream object when accessed"""
+    def __init__(self, **kwargs):
+        self._kwargs = kwargs
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, obj, type=None) -> MJPEGStream:
+        """The value of the property
+
+        If `obj` is none (i.e. we are getting the attribute of the class), 
+        we return the descriptor.
+
+        If no getter is set, we'll return either the initial value, or the value
+        from the object's __dict__, i.e. we behave like a variable.
+
+        If a getter is set, we will use it, unless the property is observable, at
+        which point the getter is only ever used once, to set the initial value.
+        """
+        if obj is None:
+            return self
+        try:
+            return obj.__dict__[self.name]
+        except KeyError:
+            obj.__dict__[self.name] = MJPEGStream(**self._kwargs)
+            return obj.__dict__[self.name]
+    
+    async def viewer_page(self, url: str) -> HTMLResponse:  # A return annotation confuses pydantic...?
+        return HTMLResponse(
+            f"<html><body><img src='{url}'></body></html>"
+        )
+
+    def add_to_fastapi(self, app: FastAPI, thing: Thing):
+        """Add the stream to the FastAPI app"""
+        app.get(
+            f"{thing.path}{self.name}",
+            response_class=MJPEGStreamResponse,
+        )(self.__get__(thing).mjpeg_stream_response)
+        @app.get(
+            f"{thing.path}{self.name}/viewer",
+            response_class=HTMLResponse,
+        )
+        async def viewer_page():
+            return await self.viewer_page(f"{thing.path}{self.name}")
+    
--- src/labthings_fastapi/utilities/__init__.py
+++ src/labthings_fastapi/utilities/__init__.py
@@ -1,7 +1,12 @@
-from typing import Any, Dict, Iterable
+from __future__ import annotations
+from typing import Any, Dict, Iterable, TYPE_CHECKING, Optional
 from weakref import WeakSet
 from pydantic import ConfigDict, Field
 from pydantic.dataclasses import dataclass
+from anyio.from_thread import BlockingPortal
+
+if TYPE_CHECKING:
+    from ..thing import Thing
 
 def class_attributes(obj: Any) -> Iterable[tuple[str, Any]]:
     """A list of all the attributes of an object's class"""
@@ -17,8 +22,13 @@ class LabThingsObjectData:
     property_observers: Dict[str, WeakSet] = Field(default_factory=dict)
 
 
-def labthings_data(obj: Any) -> LabThingsObjectData:
+def labthings_data(obj: Thing) -> LabThingsObjectData:
     """Get (or create) a dictionary for LabThings properties"""
     if LABTHINGS_DICT_KEY not in obj.__dict__:
         obj.__dict__[LABTHINGS_DICT_KEY] = LabThingsObjectData()
     return obj.__dict__[LABTHINGS_DICT_KEY]
+
+
+def get_blocking_portal(obj: Thing) -> Optional[BlockingPortal]:
+    """Retrieve a blocking portal from a Thing"""
+    return obj._labthings_blocking_portal
