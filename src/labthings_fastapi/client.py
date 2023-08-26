538ac5fd6e86739de02054057bced4bdf9607189
Richard Bowman <richard.bowman@cantab.net>
2023-08-26 22:11:48 +0100

    WIP: basic thing client

--- src/labthings_fastapi/client.py
+++ src/labthings_fastapi/client.py
@@ -8,30 +8,64 @@ Description.
 from __future__ import annotations
 from typing import Any, Optional, Union
 import httpx
+from urllib.parse import urlparse, urljoin
 
 from pydantic import BaseModel
 
-class PropertyClientDescriptor:
-    pass
+
+ACTION_RUNNING_KEYWORDS = ["idle", "pending", "running"]
+
+
+def get_link(obj: dict, rel: str) -> str:
+    """Retrieve a link from an object's `links` list, by its `rel` attribute"""
+    return next(link for link in obj["links"] if link["rel"] == rel)
+
+
+def task_href(t):
+    """Extract the endpoint address from a task dictionary"""
+    return get_link(t, "self")["href"]
+
+
+def poll_task(client, task, interval=0.5):
+    """Poll a task until it finishes, and return the return value"""
+    while task["status"] in ACTION_RUNNING_KEYWORDS:
+        r = client.get(task_href(task))
+        r.raise_for_status()
+        task = r.json()
+    return task
+
 
 class ThingClient:
-    def __init__(self, base_uri: str):
-        self.base_uri = base_uri
-        self.client = httpx.Client() 
+    """A client for a LabThings-FastAPI Thing
+    
+    NB ThingClient must be subclassed to add actions/properties,
+    so this class will be minimally useful on its own.
+    """
+    def __init__(self, base_url: str):
+        parsed = urlparse(base_url)
+        server = f"{parsed.scheme}://{parsed.netloc}"
+        self.server = server
+        self.path = parsed.path
+        self.client = httpx.Client(base_url=server) 
 
     def get_property(self, path: str) -> Any:
-        r = self.client.get(self.base_uri + path)
+        r = self.client.get(urljoin(self.path, path))
         r.raise_for_status()
         return r.json()
     
     def set_property(self, path: str, value: Any):
-        r = self.client.post(self.base_uri + path, json=value)
+        r = self.client.post(urljoin(self.path, path), json=value)
         r.raise_for_status()
 
     def invoke_action(self, path: str, **kwargs):
-        r = self.client.post(self.base_uri + path, json=kwargs)
+        r = self.client.post(urljoin(self.path, path), json=kwargs)
         r.raise_for_status()
-        return r.json()
+        poll_task(self.client, r.json())
+
+
+class PropertyClientDescriptor:
+    pass
+
 
 def property_descriptor(
         property_name: str,
@@ -80,6 +114,7 @@ def add_action(cls: type[ThingClient], action_name: str, action: dict):
     if "description" in action:
         action_method.__doc__ = action["description"]
     setattr(cls, action_name, action_method)
+
 
 def add_property(cls: type[ThingClient], property_name: str, property: dict):
     """Add a property to a ThingClient subclass"""
