4cdf8dddaaa5f1d6d65405a637acf3e02f7247b3
Richard Bowman <richard.bowman@cantab.net>
2023-11-28 14:39:55 +0000

    Supporting BlobOutput object for HTTP client

--- src/labthings_fastapi/client/outputs.py
+++ src/labthings_fastapi/client/outputs.py
@@ -8,33 +8,32 @@ class ClientBlobOutput:
 
     This object is returned by a client when the output is not serialised to JSON.
     It may be either retrieved to memory using `.content`, or saved to a file using
-    `.save_to_file()`.
+    `.save()`.
     """
 
     media_type: str
     download_url: str
 
     def __init__(
-        self, media_type: str, download_url: str, client: Optional[httpx.Client] = None
+        self, media_type: str, href: str, client: Optional[httpx.Client] = None
     ):
         self.media_type = media_type
-        self.download_url = download_url
+        self.href = href
         self.client = client or httpx.Client()
 
     @property
     def content(self) -> bytes:
         """Return the the output as a `bytes` object"""
-        return self.client.get(self.download_url).content
+        return self.client.get(self.href).content
 
-    def save_to_file(self, filepath: str) -> None:
+    def save(self, filepath: str) -> None:
         """Save the output to a file.
 
         This may remove the need to hold the output in memory, though for now it
         simply retrieves the output into memory, then writes it to a file.
         """
-        if hasattr(self, "_bytes"):
-            with open(filepath, "wb") as f:
-                f.write(self.content)
+        with open(filepath, "wb") as f:
+            f.write(self.content)
 
     def open(self) -> io.IOBase:
         """Open the output as a binary file-like object."""
