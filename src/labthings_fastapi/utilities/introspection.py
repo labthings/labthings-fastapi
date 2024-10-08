b022566872588bfc0a931c6a7a585e0f156cb7ae
Richard Bowman <richard.bowman@cantab.net>
2024-10-08 14:33:08 +0100

    Update FastAPI
    FastAPI changed the return type of its (internal) `analyze_param`
    function. I've changed my code accordingly, but have added
    a version constraint as it's not backwards-compatible.

--- src/labthings_fastapi/utilities/introspection.py
+++ src/labthings_fastapi/utilities/introspection.py
@@ -134,14 +134,14 @@ def fastapi_dependency_params(func: Callable) -> Sequence[Parameter]:
     # TODO: this currently ignores path parameters
     sig = get_typed_signature(func)
     dependencies = []
-    for param_name, param in sig.parameters.items():  # type: ignore
-        _type_annotation, depends, _param_field = analyze_param(
+    for param_name, param in sig.parameters.items():
+        param_details = analyze_param(
             param_name=param_name,
             annotation=param.annotation,
             value=param.default,
             is_path_param=False,
         )
-        if depends is not None:  # type: ignore[has-type]
+        if param_details.depends is not None:
             dependencies.append(param)
     return dependencies
 
