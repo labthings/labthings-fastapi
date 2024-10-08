8ad808c2f68f7e3e785c4e8f16e1cc6dd130a03d
Richard Bowman <richard.bowman@cantab.net>
2024-10-08 15:01:43 +0100

    Switch to pydantic_numpy
    pydantic_numpy now does everything I need it to. There's a slight difference in how we have handled 0d arrays, but I think
    this is an edge case I can ignore.
    
    I've defined, for now, an NDArray type alias that is a work-alike
    for my old class. However, this should be phased out in favour
    of `pydantic_numpy.typing` which is much better.

--- tests/test_numpy_type.py
+++ tests/test_numpy_type.py
@@ -6,13 +6,48 @@ import numpy as np
 from labthings_fastapi.types.numpy import NDArray
 
 
-def test_ndarray_field():
+def check_field_works_with_list(data):
     class Model(BaseModel):
         a: NDArray
 
-    m = Model(a=[[[1]]])
+    m = Model(a=data)
     assert isinstance(m.a, np.ndarray)
     d = m.model_dump()
-    assert d["a"] == [[[1]]]
+    assert (d["a"] == data).all()
+    m.model_json_schema()
+    m.model_dump_json()
+
+
+def check_field_works_with_ndarray(data):
+    class Model(BaseModel):
+        a: NDArray
+
+    m = Model(a=data)
+    assert isinstance(m.a, np.ndarray)
+    d = m.model_dump()
+    assert (d["a"] == data.tolist()).all()
+    m.model_json_schema()
+    m.model_dump_json()
+
+def test_1d():
+    check_field_works_with_list([1])
+    check_field_works_with_list([1, 2, 3])
+    check_field_works_with_list(np.arange(10))
+
+def test_3d():
+    check_field_works_with_list([[[1]]])
+    check_field_works_with_list([[[2]]])
+
+def test_2d():
+    check_field_works_with_list([[1, 2]])
+
+def test_0d():
+    class Model(BaseModel):
+        a: NDArray
+
+    m = Model(a=1)
+    assert m.a == 1
+    d = m.model_dump()
+    assert d["a"] == 1
     m.model_json_schema()
     m.model_dump_json()
