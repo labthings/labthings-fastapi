b04254ee4a59395c9d107b3169b487bd8dc9a8ed
Richard Bowman <richard.bowman@cantab.net>
2024-10-08 14:20:07 +0100

    Format fixes

--- src/labthings_fastapi/utilities/introspection.py
+++ src/labthings_fastapi/utilities/introspection.py
@@ -134,7 +134,7 @@ def fastapi_dependency_params(func: Callable) -> Sequence[Parameter]:
     # TODO: this currently ignores path parameters
     sig = get_typed_signature(func)
     dependencies = []
-    for param_name, param in sig.parameters.items():  #type: ignore
+    for param_name, param in sig.parameters.items():  # type: ignore
         _type_annotation, depends, _param_field = analyze_param(
             param_name=param_name,
             annotation=param.annotation,
