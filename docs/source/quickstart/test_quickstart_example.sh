eba356c1e035c4c2a74e85fd50acb5052f646741
Richard Bowman <richard.bowman@cantab.net>
2025-03-13 00:12:27 +0000

    Add test to CI and improve portability
    I fixed a few path gotchas and added the test so it runs with unpinned dependencies.

--- docs/source/quickstart/test_quickstart_example.sh
+++ docs/source/quickstart/test_quickstart_example.sh
@@ -1,15 +1,31 @@
-set -e
-
+# cd to this directory
+cd "${BASH_SOURCE%/*}/"
+rm -rf .venv
+# Override the virtual environment so we use the package
+# from this repo, not from pypi
+python -m venv .venv
+source .venv/bin/activate
+pip install ../../..
 # Run the quickstart code in the background
-bash ./quickstart_example.sh 2>&1 &
+bash "quickstart_example.sh" 2>&1 &
 quickstart_example_pid=$!
 echo "Spawned example with PID $quickstart_example_pid"
+
+function killserver {
+  # Stop the server that we spawned.
+  children=$(ps -o pid= --ppid "$quickstart_example_pid")
+  kill $children
+  echo "Killed spawned processes: $children"
+
+  wait
+}
+trap killserver EXIT
 
 # Wait for it to respond
 # Loop until the command is successful or the maximum number of attempts is reached
 ret=7
 attempt_num=0
-while [[ $ret == 7 ]] && [ $attempt_num -le 50 ]; do
+while [[ $ret == 7 ]]; do  # && [ $attempt_num -le 50 ]
   # Execute the command
   ret=0
   curl -sf -m 10 http://localhost:5000/counter/counter || ret=$?
@@ -29,22 +45,17 @@ while [[ $ret == 7 ]] && [ $attempt_num -le 50 ]; do
 done
 echo "Final return value $ret on attempt $attempt_num"
 
-# Check the Python client code
-echo "Running Python client code"
-(. .venv/bin/activate && python counter_client.py)
-
-
-# Get the spawned server's PID
-children=$(ps -o pid= --ppid "$quickstart_example_pid")
-kill $children
-echo "Killed spawned processes: $children"
-
-wait
-
 if [[ $ret == 0 ]]; then
     echo "Success"
-    exit 0
 else
     echo "Curl returned $ret, likely something went wrong."
     exit -1
+fi
+
+# Check the Python client code
+echo "Running Python client code"
+(source .venv/bin/activate && python counter_client.py)
+if [[ $? != 0 ]]; then
+  echo "Python client code did not run OK."
+  exit -1
 fi
