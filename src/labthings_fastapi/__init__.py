9de95c6ddbe4c6d0dfd870f6e4d10d60149dc5c1
Richard Bowman <richard.bowman@cantab.net>
2025-06-26 11:29:22 +0100

    Add more key symbols to top level

--- src/labthings_fastapi/__init__.py
+++ src/labthings_fastapi/__init__.py
@@ -5,6 +5,13 @@ from .decorators import (
     thing_setting,
     thing_action,
 )
+from .dependencies.blocking_portal import BlockingPortal
+from .dependencies.invocation import InvocationID, InvocationLogger
+from .dependencies.metadata import GetThingStates
+from .dependencies.raw_thing import raw_thing_dependency
+from .dependencies.thing import direct_thing_client_dependency
+from .outputs.mjpeg_stream import MJPEGStream, MJPEGStreamDescriptor
+from .outputs.blob import Blob
 
 # The symbols in __all__ are part of our public API.
 # They are imported when using `import labthings_fastapi as lt`.
@@ -19,4 +26,13 @@ __all__ = [
     "thing_property",
     "thing_setting",
     "thing_action",
+    "BlockingPortal",
+    "InvocationID",
+    "InvocationLogger",
+    "GetThingStates",
+    "raw_thing_dependency",
+    "direct_thing_client_dependency",
+    "MJPEGStream",
+    "MJPEGStreamDescriptor",
+    "Blob",
 ]
