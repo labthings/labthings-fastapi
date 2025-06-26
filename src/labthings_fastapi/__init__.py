d07599a906313925855bcde77f5b8eb9e71646d9
Richard Bowman <richard.bowman@cantab.net>
2025-06-26 11:12:41 +0100

    Add __all__

--- src/labthings_fastapi/__init__.py
+++ src/labthings_fastapi/__init__.py
@@ -1,5 +1,22 @@
 from .thing import Thing
 from .descriptors import ThingProperty, ThingSetting
 from .decorators import (
-    thing_property, thing_setting, thing_action,
+    thing_property,
+    thing_setting,
+    thing_action,
 )
+
+# The symbols in __all__ are part of our public API.
+# They are imported when using `import labthings_fastapi as lt`.
+# We should check that these symbols stay consistent if modules are rearranged.
+# The alternative `from .thing import Thing as Thing` syntax is not used, as
+# `mypy` is now happy with the current import style. If other tools prefer the
+# re-export style, we may switch in the future.
+__all__ = [
+    "Thing",
+    "ThingProperty",
+    "ThingSetting",
+    "thing_property",
+    "thing_setting",
+    "thing_action",
+]
