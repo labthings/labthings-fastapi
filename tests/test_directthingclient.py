7875d6e3dde3a91e1af8a9f18a1820428638af57
Richard Bowman <richard.bowman@cantab.net>
2025-08-12 17:14:40 +0100

    Add unit tests for DirectThingClient
    These currently check that read-only things are read-only, and
    that actions can be called but not other methods.
    
    In the future it would be good to check dependencies in here as well.

---
+++ tests/test_directthingclient.py
@@ -0,0 +1,128 @@
+"""Test the DirectThingClient class.
+
+This module tests inter-Thing interactions. It does not yet test exhaustively,
+and has been added primarily to fix #165.
+"""
+
+from fastapi.testclient import TestClient
+import pytest
+import labthings_fastapi as lt
+from labthings_fastapi.client.in_server import direct_thing_client_class
+from .temp_client import poll_task
+
+
+class Counter(lt.Thing):
+    ACTION_ONE_RESULT = "Action one result!"
+
+    @lt.thing_action
+    def increment(self) -> str:
+        """An action that takes no arguments"""
+        return self.increment_internal()
+
+    def increment_internal(self) -> str:
+        """An action that increments the counter."""
+        self.count += self.step
+        return self.ACTION_ONE_RESULT
+
+    step: int = lt.property(default=1)
+    count: int = lt.property(default=0, readonly=True)
+
+
+@pytest.fixture
+def counter_client(mocker):
+    counter = Counter()
+    counter._labthings_blocking_portal = mocker.Mock(["start_task_soon"])
+
+    CounterClient = direct_thing_client_class(Counter, "/counter")
+
+    class StandaloneCounterClient(CounterClient):
+        def __init__(self, wrapped):
+            self._dependencies = {}
+            self._request = mocker.Mock()
+            self._wrapped_thing = wrapped
+
+    return StandaloneCounterClient(counter)
+
+
+def test_readwrite_property(counter_client):
+    """Test a read/write property works as expected."""
+    counter_client.step = 2
+    assert counter_client.step == 2
+
+
+def test_readonly_property(counter_client):
+    """Test a read/write property works as expected."""
+    assert counter_client.count == 0
+    with pytest.raises(AttributeError):
+        counter_client.count = 10
+
+
+def test_action(counter_client):
+    """Test we can run an action."""
+    assert counter_client.count == 0
+    counter_client.increment()
+    assert counter_client.count == 1
+
+
+def test_method(counter_client):
+    """Methods that are not decorated as actions should be missing."""
+    with pytest.raises(AttributeError):
+        counter_client.increment_internal()
+    # Just to double-check the line above isn't a typo...
+    counter_client._wrapped_thing.increment_internal()
+
+
+CounterDep = lt.deps.direct_thing_client_dependency(Counter, "/counter/")
+RawCounterDep = lt.deps.raw_thing_dependency(Counter)
+
+
+class Controller(lt.Thing):
+    @lt.thing_action
+    def count_in_twos(self, counter: CounterDep) -> str:
+        """An action that needs a Counter and uses its affordances."""
+        counter.step = 2
+        assert counter.count == 0
+        counter.increment()
+        assert counter.count == 2
+        return "success"
+
+    @lt.thing_action
+    def count_internal(self, counter: CounterDep) -> str:
+        """Another action that tries to access local-only attributes."""
+        try:
+            counter.increment_internal()
+            raise AssertionError("Expected error was not raised!")
+        except AttributeError:
+            # pytest.raises seems to hang.
+            pass
+        try:
+            counter.count = 4
+            raise AssertionError("Expected error was not raised!")
+        except AttributeError:
+            # pytest.raises seems to hang.
+            pass
+        return "success"
+
+    @lt.thing_action
+    def count_raw(self, counter: RawCounterDep) -> str:
+        counter.count = 0
+        counter.step = -1
+        counter.increment_internal()
+        assert counter.count == -1
+        return "success"
+
+
+@pytest.mark.parametrize("action", ["count_in_twos", "count_internal", "count_raw"])
+def test_directthingclient_in_server(action):
+    """Test that a Thing can depend on another Thing
+
+    This uses the internal thing client mechanism.
+    """
+    server = lt.ThingServer()
+    server.add_thing(Counter(), "/counter")
+    server.add_thing(Controller(), "/controller")
+    with TestClient(server.app) as client:
+        r = client.post(f"/controller/{action}")
+        invocation = poll_task(client, r.json())
+        assert invocation["status"] == "completed"
+        assert invocation["output"] == "success"
