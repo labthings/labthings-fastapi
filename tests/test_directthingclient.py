d223eb7580a1f70332862c3da638ebde96801f17
Richard Bowman <richard.bowman@cantab.net>
2025-08-13 09:53:07 +0100

    Better docstrings in test_directthingclient
    I've also reordered so the classes are defined before the tests, but have not changed any code.

--- tests/test_directthingclient.py
+++ tests/test_directthingclient.py
@@ -7,7 +7,7 @@ and has been added primarily to fix #165.
 from fastapi.testclient import TestClient
 import pytest
 import labthings_fastapi as lt
-from labthings_fastapi.client.in_server import direct_thing_client_class
+from lt.deps import DirectThingClient, direct_thing_client_class
 from .temp_client import poll_task
 
 
@@ -29,7 +29,17 @@ class Counter(lt.Thing):
 
 
 @pytest.fixture
-def counter_client(mocker):
+def counter_client(mocker) -> DirectThingClient:
+    r"""Instantiate a Counter and wrap it in a DirectThingClient.
+
+    In order to make this work without a server, ``DirectThingClient`` is
+    subclassed, and ``__init__`` is overridden.
+    This could be done with ``mocker`` but it would be more verbose and
+    less clear.
+
+    :param mocker: the mocker test fixture from ``pytest-mock``\ .
+    :returns: a ``DirectThingClient`` subclass wrapping a ``Counter``\ .
+    """
     counter = Counter()
     counter._labthings_blocking_portal = mocker.Mock(["start_task_soon"])
 
@@ -42,6 +52,62 @@ def counter_client(mocker):
             self._wrapped_thing = wrapped
 
     return StandaloneCounterClient(counter)
+
+
+CounterDep = lt.deps.direct_thing_client_dependency(Counter, "/counter/")
+RawCounterDep = lt.deps.raw_thing_dependency(Counter)
+
+
+class Controller(lt.Thing):
+    """Controller is used to test a real DirectThingClient in a server.
+
+    This is used by ``test_directthingclient_in_server`` to verify the
+    client works as expected when created normally, rather than by mocking
+    the server.
+    """
+
+    @lt.thing_action
+    def count_in_twos(self, counter: CounterDep) -> str:
+        """An action that needs a Counter and uses its affordances.
+
+        This only uses methods that are part of the HTTP API, so all
+        of these commands should work.
+        """
+        counter.step = 2
+        assert counter.count == 0
+        counter.increment()
+        assert counter.count == 2
+        return "success"
+
+    @lt.thing_action
+    def count_internal(self, counter: CounterDep) -> str:
+        """An action that tries to access local-only attributes.
+
+        This previously used `pytest.raises` but that caused the test
+        to hang, most likely because this will run in a background thread.
+        """
+        try:
+            counter.increment_internal()
+            raise AssertionError("Expected error was not raised!")
+        except AttributeError:
+            # pytest.raises seems to hang.
+            pass
+        try:
+            counter.count = 4
+            raise AssertionError("Expected error was not raised!")
+        except AttributeError:
+            # pytest.raises seems to hang.
+            pass
+        return "success"
+
+    @lt.thing_action
+    def count_raw(self, counter: RawCounterDep) -> str:
+        """Increment the counter using a method that is not an Action."""
+        counter.count = 0
+        counter.step = -1
+        counter.increment_internal()
+        assert counter.count == -1
+        return "success"
 
 
 def test_readwrite_property(counter_client):
@@ -70,46 +136,6 @@ def test_method(counter_client):
         counter_client.increment_internal()
     # Just to double-check the line above isn't a typo...
     counter_client._wrapped_thing.increment_internal()
-
-
-CounterDep = lt.deps.direct_thing_client_dependency(Counter, "/counter/")
-RawCounterDep = lt.deps.raw_thing_dependency(Counter)
-
-
-class Controller(lt.Thing):
-    @lt.thing_action
-    def count_in_twos(self, counter: CounterDep) -> str:
-        """An action that needs a Counter and uses its affordances."""
-        counter.step = 2
-        assert counter.count == 0
-        counter.increment()
-        assert counter.count == 2
-        return "success"
-
-    @lt.thing_action
-    def count_internal(self, counter: CounterDep) -> str:
-        """Another action that tries to access local-only attributes."""
-        try:
-            counter.increment_internal()
-            raise AssertionError("Expected error was not raised!")
-        except AttributeError:
-            # pytest.raises seems to hang.
-            pass
-        try:
-            counter.count = 4
-            raise AssertionError("Expected error was not raised!")
-        except AttributeError:
-            # pytest.raises seems to hang.
-            pass
-        return "success"
-
-    @lt.thing_action
-    def count_raw(self, counter: RawCounterDep) -> str:
-        counter.count = 0
-        counter.step = -1
-        counter.increment_internal()
-        assert counter.count == -1
-        return "success"
 
 
 @pytest.mark.parametrize("action", ["count_in_twos", "count_internal", "count_raw"])
