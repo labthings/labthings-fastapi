be58f69a289ed7504a2bc42a118f662535246adb
Julian Stirling <julian@julianstirling.co.uk>
2025-12-14 18:06:44 +0000

    Add failing unit test for broken affordance urls

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,13 +1,12 @@
 """Test the ThingServer.
 
-Currently, this adds one trivial test for an error.
-
 While the server is covered by many of the other tests, it would
 be helpful to have some more bottom-up unit testing in this file.
 """
 
 import pytest
 import labthings_fastapi as lt
+from fastapi.testclient import TestClient
 
 
 def test_server_from_config_non_thing_error():
@@ -18,3 +17,49 @@ def test_server_from_config_non_thing_error():
                 things={"thingone": lt.ThingConfig(cls="builtins:object")}
             )
         )
+
+
+def test_server_thing_descriptions():
+    """Check the server ThingDescriptions.
+
+    Check the expected Action and Properties and their URLS are in the server
+    thing_descriptions.
+    """
+    conf = {
+        "things": {
+            "thing1": "labthings_fastapi.example_things:MyThing",
+            "thing2": {
+                "class": "labthings_fastapi.example_things:MyThing",
+                "kwargs": {},
+            },
+        }
+    }
+
+    thing_names = ["thing1", "thing2"]
+    props = ["counter", "foo"]
+    actions = [
+        "action_with_only_kwargs",
+        "action_without_arguments",
+        "anaction",
+        "increment_counter",
+        "make_a_dict",
+        "slowly_increase_counter",
+    ]
+
+    server = lt.ThingServer.from_config(conf)
+    with TestClient(server.app) as client:
+        response = client.get("/thing_descriptions/")
+    response.raise_for_status()
+    thing_descriptions = response.json()
+
+    for thing_name in thing_names:
+        thing_description = thing_descriptions[thing_name]
+        for action_name in actions:
+            action = thing_description["actions"][action_name]
+            expected_href = thing_name + "/" + action_name
+            assert action["forms"][0]["href"] == expected_href
+
+        for prop_name in props:
+            prop = thing_description["properties"][prop_name]
+            expected_href = thing_name + "/" + prop_name
+            assert prop["forms"][0]["href"] == expected_href
