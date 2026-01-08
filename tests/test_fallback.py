128aa79c627be4d866d1e95e9684486a112f150f
Julian Stirling <julian@julianstirling.co.uk>
2026-01-08 10:46:56 +0000

    Add Failing unit test for the fallback server when passing the configuration object

--- tests/test_fallback.py
+++ tests/test_fallback.py
@@ -5,9 +5,21 @@ we start a lightweight fallback server to show an error message. This test
 verifies that it works as expected.
 """
 
+import re
+
 from fastapi.testclient import TestClient
 import labthings_fastapi as lt
 from labthings_fastapi.server.fallback import app
+
+CONFIG_DICT = {
+    "things": {
+        "thing1": "labthings_fastapi.example_things:MyThing",
+        "thing2": {
+            "class": "labthings_fastapi.example_things:MyThing",
+            "kwargs": {},
+        },
+    }
+}
 
 
 def test_fallback_empty():
@@ -19,14 +31,31 @@ def test_fallback_empty():
         assert "No logging info available" in html
 
 
-def test_fallback_with_config():
-    app.labthings_config = {"hello": "goodbye"}
+def test_fallback_with_config_dict():
+    """Check that fallback server prints a config dictionary as JSON."""
+    app.labthings_config = CONFIG_DICT
     with TestClient(app) as client:
         response = client.get("/")
         html = response.text
         assert "Something went wrong" in html
         assert "No logging info available" in html
-        assert '"hello": "goodbye"' in html
+        assert '"thing1": "labthings_fastapi.example_things:MyThing"' in html
+        assert '"class": "labthings_fastapi.example_things:MyThing"' in html
+
+
+def test_fallback_with_config_obj():
+    """Check that fallback server prints the config object as JSON."""
+    config = lt.ThingServerConfig.model_validate(CONFIG_DICT)
+    app.labthings_config = config
+    with TestClient(app) as client:
+        response = client.get("/")
+        html = response.text
+        assert "Something went wrong" in html
+        assert "No logging info available" in html
+        assert "thing1" in html
+        assert "thing2" in html
+        cls_regex = re.compile(r'"cls": "labthings_fastapi\.example_things\.MyThing"')
+        assert len(cls_regex.findall(html)) == 2
 
 
 def test_fallback_with_error():
@@ -41,16 +70,7 @@ def test_fallback_with_error():
 
 
 def test_fallback_with_server():
-    config_dict = {
-        "things": {
-            "thing1": "labthings_fastapi.example_things:MyThing",
-            "thing2": {
-                "class": "labthings_fastapi.example_things:MyThing",
-                "kwargs": {},
-            },
-        }
-    }
-    config = lt.ThingServerConfig.model_validate(config_dict)
+    config = lt.ThingServerConfig.model_validate(CONFIG_DICT)
     app.labthings_server = lt.ThingServer.from_config(config)
     with TestClient(app) as client:
         response = client.get("/")
