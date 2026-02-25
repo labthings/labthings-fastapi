5d2e00ecaea610225a7a9f22fd5d4eab6edb128c
Richard Bowman <richard.bowman@cantab.net>
2026-02-25 23:24:37 +0000

    Add an API prefix
    It's now possible to configure an API prefix, which affects all LabThings-generated URLs.
    
    I've also switched a couple of places from passing the app around to creating an APIRouter, which feels much cleaner.
    
    So far, tests pass but I've not tried to set a prefix.

--- src/labthings_fastapi/server/__init__.py
+++ src/labthings_fastapi/server/__init__.py
@@ -11,7 +11,7 @@ from typing import Any, AsyncGenerator, Optional, TypeVar
 from typing_extensions import Self
 import os
 
-from fastapi import FastAPI, Request
+from fastapi import APIRouter, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from anyio.from_thread import BlockingPortal
 from contextlib import asynccontextmanager, AsyncExitStack
@@ -65,6 +65,7 @@ class ThingServer:
         self,
         things: ThingsConfig,
         settings_folder: Optional[str] = None,
+        api_prefix: str = "",
         application_config: Optional[Mapping[str, Any]] = None,
     ) -> None:
         r"""Initialise a LabThings server.
@@ -82,8 +83,9 @@ class ThingServer:
             arguments, and any connections to other `.Thing`\ s.
         :param settings_folder: the location on disk where `.Thing`
             settings will be saved.
+        :param api_prefix: An optional prefix for all API routes. This must either
+            be empty, or start with a slash and not end with a slash.
         :param application_config: A mapping containing custom configuration for the
-            application. This is not processed by LabThings. Each `.Thing` can access
             application. This is not processed by LabThings. Each `.Thing` can access
             this via the Thing-Server interface.
         """
@@ -99,9 +101,9 @@ class ThingServer:
         self._set_url_for_middleware()
         self.settings_folder = settings_folder or "./settings"
         self.action_manager = ActionManager()
-        self.action_manager.attach_to_app(self.app)
-        self.app.include_router(blob.router)  # include blob download endpoint
-        self._add_things_view_to_app()
+        self.app.include_router(self.action_manager.router(), prefix=self._api_prefix)
+        self.app.include_router(blob.router, prefix=self._api_prefix)
+        self.app.include_router(self._things_view_router(), prefix=self._api_prefix)
         self.blocking_portal: Optional[BlockingPortal] = None
         self.startup_status: dict[str, str | dict] = {"things": {}}
         global _thing_servers  # noqa: F824
@@ -166,6 +168,15 @@ class ThingServer:
         """
         return self._config.application_config
 
+    @property
+    def _api_prefix(self) -> str:
+        """A string that prefixes all URLs in the application.
+
+        This must either be empty, or start with a slash and not
+        end with a slash.
+        """
+        return self._config.api_prefix
+
     ThingInstance = TypeVar("ThingInstance", bound=Thing)
 
     def things_by_class(self, cls: type[ThingInstance]) -> Sequence[ThingInstance]:
@@ -209,7 +220,7 @@ class ThingServer:
         """
         if name not in self._things:
             raise KeyError(f"No thing named {name} has been added to this server.")
-        return f"/{name}/"
+        return f"{self._api_prefix}/{name}/"
 
     def _create_things(self) -> Mapping[str, Thing]:
         r"""Create the Things, add them to the server, and connect them up if needed.
@@ -317,15 +328,14 @@ class ThingServer:
 
         self.blocking_portal = None
 
-    def _add_things_view_to_app(self) -> None:
-        """Add an endpoint that shows the list of attached things."""
+    def _things_view_router(self) -> APIRouter:
+        """Create a router for the endpoint that shows the list of attached things.
+
+        :returns: an APIRouter with the `thing_descriptions` endpoint.
+        """
+        router = APIRouter()
         thing_server = self
 
-        @self.app.get(
-            "/thing_descriptions/",
-            response_model_exclude_none=True,
-            response_model_by_alias=True,
-        )
         def thing_descriptions(request: Request) -> Mapping[str, ThingDescription]:
             """Describe all the things available from this server.
 
@@ -345,6 +355,15 @@ class ThingServer:
                 name: thing.thing_description(name + "/", base=str(request.base_url))
                 for name, thing in thing_server.things.items()
             }
+
+        router.add_api_route(
+            "/thing_descriptions/",
+            thing_descriptions,
+            response_model_exclude_none=True,
+            response_model_by_alias=True,
+        )
+
+        return router
 
         @self.app.get("/things/")
         def thing_paths(request: Request) -> Mapping[str, str]:
