d15b95209166aad7da59998f5ed6b6cb11a8cbea
Richard Bowman <richard.bowman@cantab.net>
2026-06-24 09:56:40 +0100

    More explicit test for status code
    I've added a test case with a non-default status code.

--- tests/test_problem_details.py
+++ tests/test_problem_details.py
@@ -7,45 +7,39 @@ from labthings_fastapi import problem_details, exceptions
 DOCS_URL = (
     "https://labthings-fastapi.readthedocs.io/en/latest/"
     "autoapi/labthings_fastapi/exceptions/index.html"
+    "#labthings_fastapi.exceptions."
 )
+PYTHON_DOCS = "https://docs.python.org/3/library/exceptions.html"
 
 
 class CustomError(Exception):
     """A custom exception."""
 
 
-ERROR_URLS = [
+ERRORS = [
     # Some standard-library exceptions
-    (BaseException, "https://docs.python.org/3/library/exceptions.html#BaseException"),
-    (Exception, "https://docs.python.org/3/library/exceptions.html#Exception"),
-    (RuntimeError, "https://docs.python.org/3/library/exceptions.html#RuntimeError"),
+    (BaseException, f"{PYTHON_DOCS}#BaseException", 500),
+    (Exception, f"{PYTHON_DOCS}#Exception", 500),
+    (RuntimeError, f"{PYTHON_DOCS}#RuntimeError", 500),
     # Some LabThings exceptions
-    (
-        exceptions.FeatureNotAvailableError,
-        f"{DOCS_URL}#labthings_fastapi.exceptions.FeatureNotAvailableError",
-    ),
-    (
-        exceptions.InvocationCancelledError,
-        f"{DOCS_URL}#labthings_fastapi.exceptions.InvocationCancelledError",
-    ),
+    (exceptions.FeatureNotAvailableError, f"{DOCS_URL}FeatureNotAvailableError", 500),
+    (exceptions.InvocationCancelledError, f"{DOCS_URL}InvocationCancelledError", 500),
+    (exceptions.GlobalLockBusyError, f"{DOCS_URL}GlobalLockBusyError", 409),
     # Custom exceptions should be None
-    (CustomError, None),
+    (CustomError, None, 500),
 ]
 
 
-@pytest.mark.parametrize(("err", "url"), ERROR_URLS)
-def test_docs_url(err, url):
+@pytest.mark.parametrize(("err", "url", "_code"), ERRORS)
+def test_docs_url(err, url, _code):
     """Check URLs for built-in and LabThings errors, and that we get None for others."""
     assert problem_details.docs_url(err) == url
 
 
-@pytest.mark.parametrize(("err", "url"), ERROR_URLS)
-def test_pd_from_exception(err, url):
+@pytest.mark.parametrize(("err", "url", "code"), ERRORS)
+def test_pd_from_exception(err, url, code):
     pd = problem_details.ProblemDetails.from_exception(err("Message"))
     assert pd.type == url
     assert pd.detail == "Message"
     assert pd.title == err.__name__
-    try:
-        assert pd.status == err.status_code
-    except AttributeError:
-        assert pd.status == 500
+    assert pd.status == code
