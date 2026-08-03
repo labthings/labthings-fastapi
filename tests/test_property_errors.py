b2de312d42600eeac1c68fac2aafa0408c2bc7c4
Richard Bowman <richard.bowman@cantab.net>
2026-08-03 11:22:01 +0100

    Test code for property errors
    This ought to be a helpful way to check when I've improved the error handling in properties.
    This is in response to #386.
    
    Unfortunately, too many of these tests already pass: this is an annoying artifact of the way
    TestClient is implemented.

---
+++ tests/test_property_errors.py
@@ -0,0 +1,122 @@
+"""Test how errors in property getters/setters are handled.
+
+This test file is intended to check the error handling code dealing with properties.
+Errors in property getter/setter code should result in helpful HTTP errors and
+Python exceptions. This test module is intended to check that various error
+conditions result in easily-interpreted exceptions, log entries, and HTTP responses.
+"""
+
+import pytest
+
+import labthings_fastapi as lt
+from labthings_fastapi.testing import create_thing_without_server
+
+
+class SpecificError(RuntimeError):
+    """A specific error class we can test for."""
+
+
+class ErrorThing(lt.Thing):
+    """A Thing that has properties with a variety of failure modes."""
+
+    @lt.property
+    def always_raises(self) -> int:
+        """An integer that errors on get and set."""
+        raise SpecificError("'always_raises' failed, as expected.")
+
+    @always_raises.setter
+    def _set_always_raises(self, value: int) -> None:
+        raise SpecificError("'always_raises' failed, as expected.")
+
+    @lt.property
+    def wrongly_typed(self) -> int:
+        """A value of the wrong type."""
+        return "not an integer"
+
+    @lt.property
+    def violates_constraint(self) -> int:
+        """An integer that's outside the allowed range."""
+        return 42
+
+    violates_constraint.constraints = {"le": 10}
+
+
+@pytest.fixture
+def thing():
+    """A fixture returning an instance of ErrorThing."""
+    return create_thing_without_server(ErrorThing)
+
+
+@pytest.fixture
+def client():
+    """A fixture returning a ThingClient for an ErrorThing."""
+    server = lt.ThingServer.from_things({"thing": ErrorThing})
+    with server.test_client() as tc:
+        return lt.ThingClient.from_url("/thing/", client=tc)
+
+
+# The functions below check errors when called directly from Python
+
+
+def test_get_always_raises_python(thing: ErrorThing):
+    """Check always_raises errors when retrieved directly."""
+    with pytest.raises(SpecificError, match="failed, as expected"):
+        _ = thing.always_raises
+
+
+def test_set_always_raises_python(thing: ErrorThing):
+    """Check always_raises errors when set directly."""
+    with pytest.raises(SpecificError, match="failed, as expected"):
+        thing.always_raises = 42
+
+
+def test_wrongly_typed_python(thing: ErrorThing):
+    """Check we can retrieve a wrongly typed value in Python."""
+    # Currently, no validation is performed on property values
+    # when they are retrieved from Python
+    assert thing.wrongly_typed == "not an integer"
+
+
+def test_violates_constraint_python(thing: ErrorThing):
+    """Check we can retrieve a value in Python that violates its constraint."""
+    assert thing.violates_constraint == 42
+
+
+# The next section tests the same conditions as the previous one, but in the
+# context of a server.
+# Typing note: `client` is type hinted as an ErrorThing as it should have an
+# equivalent signature. This is largely to enable static analysis in editors.
+# It is actually a `lt.ThingClient` instance.
+
+
+def test_get_always_raises_server(client: ErrorThing):
+    """Check always_raises errors nicely when retrieved over HTTP."""
+    with pytest.raises(SpecificError, match="failed, as expected"):
+        _ = client.always_raises
+
+
+def test_set_always_raises_server(client: ErrorThing):
+    """Check always_raises errors when set over HTTP."""
+    with pytest.raises(SpecificError, match="failed, as expected"):
+        client.always_raises = 42
+
+
+def test_wrongly_typed_server(client: ErrorThing):
+    """Check the error when we return a wrongly typed value."""
+    assert client.wrongly_typed == "not an integer"
+
+
+def test_violates_constraint_server(client: ErrorThing):
+    """Check we can retrieve a value that violates its constraint.
+
+    Constraints are not yet validated on the return values of property
+    getters.
+    """
+    assert client.violates_constraint == 42
+
+
+if __name__ == "__main__":
+    # This block enables the test Thing here to be interacted with over
+    # HTTP, to allow manual testing from a variety of clients.
+    server = lt.ThingServer.from_things({"thing": ErrorThing})
+    server.serve()
